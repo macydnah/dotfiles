2e528212e5e9b9ac2ca217837d744a253d1c332d
macydnah <86342863+macydnah@users.noreply.github.com>
2025-09-06 22:59:19 -0600

    feat(neovim): navigate splits with `ctrl-[hjkl]`

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -78,6 +78,24 @@ if diff_mode then
   set.diffopt = { "filler", "context:1000000" }
 end
 
+--[[ General Mappings ]]
+
+-- Use ctrl-[hjkl] to select the active split
+-- http://vim.wikia.com/wiki/Switch_between_Vim_window_splits_easily
+-- https://stackoverflow.com/questions/6053301/easier-way-to-navigate-between-vim-split-panes
+map({'n'}, '<c-h>', function()
+  vim.cmd('wincmd h')
+end, { desc = "Move cursor to Nth window left of current one", silent = true })
+map({'n'}, '<c-j>', function()
+  vim.cmd('wincmd j')
+end, { desc = "Move cursor to Nth window below current one", silent = true })
+map({'n'}, '<c-k>', function()
+  vim.cmd('wincmd k')
+end, { desc = "Move cursor to Nth window above current one", silent = true })
+map({'n'}, '<c-l>', function()
+  vim.cmd('wincmd l')
+end, { desc = "Move cursor to Nth window right of current one", silent = true })
+
 --[[ General Autocommands ]]
 autocmd('TextYankPost', {
   desc = "Briefly highlight yanked text",

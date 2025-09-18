aaf783e6984f15b16b1ae3f622ca604dd57e438f
macydnah <86342863+macydnah@users.noreply.github.com>
2025-09-17 19:59:48 -0600

    feat(neovim): add `hjkl_split_navigation.lua`

---
+++ config/nvim/after/plugin/hjkl_split_navigation.lua
@@ -0,0 +1,20 @@
+---[[ ~/.config/nvim/after/plugin/hjkl_split_navigation.lua
+
+-- Use ctrl-[hjkl] to select the active split
+
+-- http://vim.wikia.com/wiki/Switch_between_Vim_window_splits_easily
+-- https://stackoverflow.com/questions/6053301/easier-way-to-navigate-between-vim-split-panes
+
+vim.keymap.set({'n'}, '<c-h>', function() vim.cmd('wincmd h') end,
+  { desc = "Move cursor to Nth window left of current one", silent = true })
+
+vim.keymap.set({'n'}, '<c-j>', function() vim.cmd('wincmd j') end,
+  { desc = "Move cursor to Nth window below current one", silent = true })
+
+vim.keymap.set({'n'}, '<c-k>', function() vim.cmd('wincmd k') end,
+  { desc = "Move cursor to Nth window above current one", silent = true })
+
+vim.keymap.set({'n'}, '<c-l>', function() vim.cmd('wincmd l') end,
+  { desc = "Move cursor to Nth window right of current one", silent = true })
+
+--]]

2151f15e41a0035217a6be6d08a1ad09005a8fc4
macydnah <86342863+macydnah@users.noreply.github.com>
2025-09-17 19:52:20 -0600

    feat(neovim): manage skeletons with `skeleton.lua`

---
+++ config/nvim/after/plugin/skeleton.lua
@@ -0,0 +1,31 @@
+---[[ ~/.config/nvim/after/plugin/skeleton.lua
+
+-- Insert a template in buffer when a editing a new file
+-- that has a skeleton template in ~/Templates (e.g. ~/Templates/skeleton.sh)
+
+local enable_filetype_skeleton = {
+  'html',
+  'xml',
+  'sh',
+  'svg',
+}
+
+local skel_group = vim.api.nvim_create_augroup('FileTypeSkeleton', { clear = true })
+
+local function insert_skeleton(filetype)
+  vim.api.nvim_create_autocmd('BufNewFile', {
+    desc = "Insert skeleton " .. filetype .. " template",
+    group = skel_group,
+    pattern = '*.' .. filetype,
+    callback = function()
+      local dir = os.getenv('HOME') .. '/Templates/skeleton' .. '.'
+      vim.cmd('0r ' .. dir .. filetype)
+    end,
+  })
+end
+
+for _, filetype in ipairs(enable_filetype_skeleton) do
+  insert_skeleton(filetype)
+end
+
+--]]

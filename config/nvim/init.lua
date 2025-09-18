1efaf82cc4834fa1b4af850781a0bd998ebcf69b
macydnah <86342863+macydnah@users.noreply.github.com>
2025-09-17 20:05:42 -0600

    feat(neovim): add `import_gsettings_on_save.lua`

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -89,6 +89,7 @@ end
 
 --[[ General Mappings ]]
 
+--[[
 -- Use ctrl-[hjkl] to select the active split
 -- http://vim.wikia.com/wiki/Switch_between_Vim_window_splits_easily
 -- https://stackoverflow.com/questions/6053301/easier-way-to-navigate-between-vim-split-panes
@@ -104,6 +105,7 @@ end, { desc = "Move cursor to Nth window above current one", silent = true })
 map({'n'}, '<c-l>', function()
   vim.cmd('wincmd l')
 end, { desc = "Move cursor to Nth window right of current one", silent = true })
+--]]
 
 --[[ General Autocommands ]]
 autocmd('TextYankPost', {

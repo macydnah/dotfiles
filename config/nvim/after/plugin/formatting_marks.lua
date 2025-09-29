6583418732be0ba664e6ec866754b59bff99ab6a
macydnah <86342863+macydnah@users.noreply.github.com>
2025-09-28 23:53:15 -0600

    refactor(neovim): space `leadmultispace` by `shiftwidth`

--- config/nvim/after/plugin/formatting_marks.lua
+++ config/nvim/after/plugin/formatting_marks.lua
@@ -8,21 +8,25 @@ vim.opt.listchars = {
   -- space = '·',
   -- multispace = '·',
   lead = '⎽',
+  -- leadmultispace = '│   ',
   trail = '·',
   extends = '»',
   precedes = '«',
   nbsp = '⍽',
 }
 
-local group = vim.api.nvim_create_augroup('FormattingMarks', { clear = true })
+local function __spacing_by(shiftwidth)
+  return string.rep(' ', shiftwidth - 1)
+end
+
+local __group = vim.api.nvim_create_augroup('FormattingMarks', { clear = true })
 vim.api.nvim_create_autocmd('BufEnter', {
-  group = group,
+  group = __group,
   pattern = '*',
   callback = function()
     if vim.opt_local.expandtab:get() then
       local shiftwidth = vim.opt_local.shiftwidth:get()
-      local spaces = string.rep(' ', shiftwidth - 1)
-      vim.opt_local.listchars:append({ leadmultispace = '│' .. spaces })
+      vim.opt_local.listchars:append({ leadmultispace = '│' .. __spacing_by(shiftwidth) })
     end
   end,
 })

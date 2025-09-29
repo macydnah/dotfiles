03b57120522d7bb19b47c104072af39f466b9a71
macydnah <86342863+macydnah@users.noreply.github.com>
2025-09-28 23:56:23 -0600

    fix(neovim): set `tex_flavor` to `latex`
    - Use same settings from `tex` for `plaintex`

--- config/nvim/after/ftplugin/tex.lua
+++ config/nvim/after/ftplugin/tex.lua
@@ -2,6 +2,9 @@
 
 -- TEX FileType settings ts=8 sts=8 sw=8 noet noai wrap linebreak smoothscroll spell
 
+vim.g.tex_flavor = 'latex'
+vim.g.tex_comment_nospell = 1
+
 vim.opt_local.tabstop = 8
 vim.opt_local.softtabstop = 8
 vim.opt_local.shiftwidth = 8
@@ -17,5 +20,22 @@ vim.opt_local.spell = true
 
 vim.opt_local.cursorline = true
 vim.opt_local.cursorcolumn = false
+vim.opt_local.list = true
+
+vim.api.nvim_create_autocmd({'BufWinEnter', 'WinEnter'}, {
+  desc = "Set background color based on time of the day",
+  group = vim.api.nvim_create_augroup('AutoLightDarkBackground', { clear = true }),
+  pattern = '*',
+  callback = function()
+    local hour = tonumber(os.date('%H%M'))
+    if hour < 1500 and hour > 0730 then
+      vim.cmd.colorscheme('lunaperche')
+      vim.opt.background = 'light'
+    else
+      vim.cmd.colorscheme('PaperColor')
+      vim.opt.background = 'dark'
+    end
+  end,
+})
 
 --]]

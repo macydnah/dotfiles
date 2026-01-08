cce4b8318144942492319e3ab0c7fa036b8e87ec
macydnah <86342863+macydnah@users.noreply.github.com>
2026-01-07 22:03:53 -0600

    feat(neovim): add java `after/ftplugin` settings

---
+++ config/nvim/after/ftplugin/java.lua
@@ -0,0 +1,19 @@
+---[[ ~/.config/nvim/after/ftplugin/java.lua
+
+-- JAVA FileType settings ts=8 sts=4 sw=4 et ai
+
+vim.opt_local.tabstop = 8
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
+
+vim.opt_local.expandtab = true
+vim.opt_local.autoindent = true
+
+local _group = vim.api.nvim_create_augroup('JdtlsHighlightOverrides', { clear = true })
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = "Link @type.builtin.java to Type highlight group",
+  group = _group,
+  callback = function()
+    vim.api.nvim_set_hl(0, "@type.builtin.java", { link = "Type" })
+  end,
+})

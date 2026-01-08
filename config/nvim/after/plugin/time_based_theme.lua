3656af5005a46250c827baf1122c8df818540b1a
macydnah <86342863+macydnah@users.noreply.github.com>
2026-01-07 21:58:39 -0600

    refactor(neovim): change colorscheme based on time of the day
    - Move config to a dedicated config plugin

---
+++ config/nvim/after/plugin/time_based_theme.lua
@@ -0,0 +1,32 @@
+---[[ ~/.config/nvim/after/plugin/time_based_theme.lua
+
+-- Set colorscheme (and/or background color) based on time of the day
+
+vim.g.manual_colorscheme = nil
+
+if vim.g.colorscheme_timer == nil then
+
+  vim.g.colorscheme_timer = true
+
+  local function update_colorscheme()
+
+    if vim.g.manual_colorscheme then
+      return
+    end
+
+    local hour = tonumber(os.date('%H%M'))
+
+    if hour < 1600 and hour > 0730 then
+      vim.cmd('colorscheme PaperColorSlimLight')
+      vim.opt.background = 'light'
+    else
+      vim.cmd('colorscheme PaperColorSlim')
+      vim.opt.background = 'dark'
+    end
+
+    vim.defer_fn(update_colorscheme, 5*60*1000)
+  end
+
+  update_colorscheme()
+end
+--]]

581415092728262825b078e7a686f5068f0747d9
macydnah <86342863+macydnah@users.noreply.github.com>
2026-01-07 22:04:57 -0600

    feat(neovim): add markdown `after/ftplugin` settings

---
+++ config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,23 @@
+---[[ ~/.config/nvim/after/ftplugin/markdown.lua
+
+-- MD FileType settings ts=8 sts=8 sw=8 et ai wrap linebreak smoothscroll spell
+
+vim.opt_local.tabstop = 8
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
+
+vim.opt_local.expandtab = true
+vim.opt_local.autoindent = true
+
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.smoothscroll = true
+
+vim.opt_local.spell = true
+
+vim.opt_local.cursorline = true
+vim.opt_local.cursorcolumn = false
+
+vim.opt_local.conceallevel = 2
+
+--]]

7b74077b192de9b20cd4ebc00f21737723d342f1
macydnah <86342863+macydnah@users.noreply.github.com>
2026-01-12 00:35:35 -0600

    fix(yazi): update changed/deprecated configs for `v26.1.4`
    - Rename the `[completion]` component to `[cmp]` (https://github.com/sxyazi/yazi/pull/2399)
    
    - Increase central panel `mgr` ratio
    
    - Rename name to url for open, fetchers, spotter, preloadres,
      previewers, filetype, and globs icon rules to support virtual file
      system (https://github.com/sxyazi/yazi/pull/3034)
    
    - Rename mime fetcher to mime.local, and introduce mime.dir fetcher to
      support folder MIME types (https://github.com/sxyazi/yazi/pull/3222)
    
    - Remove $0 parameter in opener rules to make the open command work
      under empty directories (https://github.com/sxyazi/yazi/pull/3226)
    
    - Deprecate $n, $@ (*nix) and %n, %* (Windows) in shell command and
      opener rules in favor of new shell formatting (https://github.com/sxyazi/yazi/pull/3232)
    
    - Make some style cleaning

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,6 +1,6 @@
 -- [Cross-instance Yank](https://yazi-rs.github.io/docs/dds/#session.lua)
 require("session"):setup {
-	sync_yanked = true,
+  sync_yanked = true,
 }
 
 -- [Folder-specific rules](https://yazi-rs.github.io/docs/tips/#folder-rules)
@@ -9,27 +9,27 @@ require("folder-rules"):setup()
 -- [Show full border](https://github.com/yazi-rs/plugins/tree/main/full-border.yazi#usage)
 --[[
 require("full-border"):setup {
-	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
-	type = ui.Border.ROUNDED,
+  -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+  type = ui.Border.ROUNDED,
 }
 --]]
 
 -- [Show username and hostname in header](https://yazi-rs.github.io/docs/tips/#username-hostname-in-header)
 --[[
 Header:children_add(function()
-	if ya.target_family() ~= "unix" then
-		return ui.Line {}
-	end
-	return ui.Span("[" .. ya.user_name() .. "@" .. ya.host_name() .. "]"):fg("green")
+  if ya.target_family() ~= "unix" then
+    return ui.Line {}
+  end
+  return ui.Span("[" .. ya.user_name() .. "@" .. ya.host_name() .. "]"):fg("green")
 end, 500, Header.LEFT)
 --]]
 
 -- [Show symlink in status bar](https://yazi-rs.github.io/docs/tips/#symlink-in-status)
 Status:children_add(function(self)
-	local h = self._current.hovered
-	if h and h.link_to then
-		return " -> " .. tostring(h.link_to)
-	else
-		return ""
-	end
+  local h = self._current.hovered
+  if h and h.link_to then
+    return " -> " .. tostring(h.link_to)
+  else
+    return ""
+  end
 end, 3300, Status.LEFT)

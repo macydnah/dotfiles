38c83ef0749ad061e139ae1b073c18098819dec8
macydnah <86342863+macydnah@users.noreply.github.com>
2026-01-11 23:26:55 -0600

    feat(neovim): add toml `after/ftplugin` settings

---
+++ config/nvim/after/ftplugin/toml.lua
@@ -0,0 +1,12 @@
+---[[ ~/.config/nvim/after/ftplugin/toml.lua
+
+-- TOML FileType settings ts=8 sts=4 sw=4 et ai
+
+vim.opt_local.tabstop = 8
+vim.opt_local.softtabstop = 4
+vim.opt_local.shiftwidth = 4
+
+vim.opt_local.expandtab = true
+vim.opt_local.autoindent = true
+
+--]]

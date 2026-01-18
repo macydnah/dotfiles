bab14ab48fb3a26cf8b6f60e75bda1c4434c20f3
macydnah <86342863+macydnah@users.noreply.github.com>
2026-01-18 16:41:53 -0600

    refactor(neovim): add wrapped lines friendly navigation

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -9,15 +9,32 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
 vim.opt_local.autoindent = true
 
-vim.opt_local.wrap = true
-vim.opt_local.linebreak = true
-vim.opt_local.smoothscroll = true
-
 vim.opt_local.spell = true
 
 vim.opt_local.cursorline = true
 vim.opt_local.cursorcolumn = false
 
 vim.opt_local.conceallevel = 2
+
+local bufnr = vim.api.nvim_get_current_buf()
+---Set a keymap for the current buffer.
+---@param mode string|string[] Mode "short-name" (see nvim_set_keymap()), or a list thereof.
+---@param lhs string Left-hand side |{lhs}| of the mapping.
+---@param desc string Description of the mapping.
+---@param rhs string|function Right-hand side |{rhs}| of the mapping, can be a Lua function.
+---@param opts table? Additional options for the mapping.
+local function bufmap(mode, lhs, desc, rhs, opts)
+  local defaults = { buffer = bufnr, desc = 'TXT: ' .. desc }
+  opts = vim.tbl_extend('force', defaults, opts or {})
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+vim.opt_local.wrap = true
+vim.opt_local.smoothscroll = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+
+bufmap('n', 'j', "Move down a visual line for wrap enabled lines", 'gj')
+bufmap('n', 'k', "Move up a visual line for wrap enabled lines", 'gk')
 
 --]]
--- config/nvim/after/ftplugin/text.lua
+++ config/nvim/after/ftplugin/text.lua
@@ -9,13 +9,30 @@ vim.opt_local.shiftwidth = 8
 vim.opt_local.expandtab = false
 vim.opt_local.autoindent = true
 
-vim.opt_local.wrap = true
-vim.opt_local.linebreak = true
-vim.opt_local.smoothscroll = true
+vim.opt_local.cursorline = true
+vim.opt_local.cursorcolumn = false
 
 vim.opt_local.spell = true
 
-vim.opt_local.cursorline = true
-vim.opt_local.cursorcolumn = false
+local bufnr = vim.api.nvim_get_current_buf()
+---Set a keymap for the current buffer.
+---@param mode string|string[] Mode "short-name" (see nvim_set_keymap()), or a list thereof.
+---@param lhs string Left-hand side |{lhs}| of the mapping.
+---@param desc string Description of the mapping.
+---@param rhs string|function Right-hand side |{rhs}| of the mapping, can be a Lua function.
+---@param opts table? Additional options for the mapping.
+local function bufmap(mode, lhs, desc, rhs, opts)
+  local defaults = { buffer = bufnr, desc = 'TXT: ' .. desc }
+  opts = vim.tbl_extend('force', defaults, opts or {})
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+vim.opt_local.wrap = true
+vim.opt_local.smoothscroll = true
+vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
+
+bufmap('n', 'j', "Move down a visual line for wrap enabled lines", 'gj')
+bufmap('n', 'k', "Move up a visual line for wrap enabled lines", 'gk')
 
 --]]
